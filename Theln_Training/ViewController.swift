cd933304719ad10eb9dac585f0fbdeb45a61ab0c
theln <theln@ttc-solutions.com.vn>
2020-10-02 14:26:56 +0700

    Initial Commit

---
+++ Theln_Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Theln_Training
+//
+//  Created by Ngoc The on 10/2/20.
+//  Copyright © 2020 Ngoc The. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Theln_TrainingTests/Theln_TrainingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Theln_TrainingTests.swift
+//  Theln_TrainingTests
+//
+//  Created by Ngoc The on 10/2/20.
+//  Copyright © 2020 Ngoc The. All rights reserved.
+//
+
+import XCTest
+@testable import Theln_Training
+
+class Theln_TrainingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
